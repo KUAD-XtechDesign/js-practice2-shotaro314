b398583bb63163f71edb285f14261bce31d0b5ef
shotaro314 <85724397+shotaro314@users.noreply.github.com>
2021-10-15 17:39:59 +0900

    sum

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,6 +1,40 @@
 $(function(){
+    
+    let txt1 ="おはようございます！"
+    let txt2 ="おげんきですか？"
+    let num1 = 20;
+    let num2 = 32;
+
+    function sayHello(){
+    $("#content").text("こんにちは！");
+    }
+
+    function saySomething(txt){
+      $("#content"). text(txt);
+    }
+
+    function samTwo (arg1, arg2){
+     $("#content"). text(arg1 + arg2);
+    }
 
 
+    $("#button01").on("click",function(){
+        sayHello();
+    })
+    $("#button02").on("click",function(){
+     saySomething(txt1);
+    })
 
+    $("#button03").on("click",function(){
+      samTwo (txt1, txt2);
+    })
 
+    $("#button04").on("click",function (){
+      samTwo (num1, num2);
+    })
+
+    $("#button05").on("click",function(){
+        samTwo(txt1,txt2);
+        samTwo(nam1,num2);
+    })
 })
